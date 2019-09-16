f488d3379febc23f6245661637561e69cafde30c
Andy Ren <andyr914@gmail.com>
2019-09-15 21:38:57 -0700

    Updates

---
+++ precache-manifest.6de889720b89aee3a6e524a033974a49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d299596c0faeaa4b5c01a17d1ab239",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "6b88e29879293718e992",
+    "url": "/ClickyGame/static/css/main.1efc79a3.chunk.css"
+  },
+  {
+    "revision": "74446349f974f97784a4",
+    "url": "/ClickyGame/static/js/2.18ca4dbb.chunk.js"
+  },
+  {
+    "revision": "6b88e29879293718e992",
+    "url": "/ClickyGame/static/js/main.279ca5b8.chunk.js"
+  },
+  {
+    "revision": "dc8e0b9f3accfa92bbb9",
+    "url": "/ClickyGame/static/js/runtime~main.3f11a19d.js"
+  }
+]);
